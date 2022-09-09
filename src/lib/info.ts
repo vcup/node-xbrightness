26c5116baecbb21d019427260d892323d7b93d09
miryo <miryo@vcup.moe>
2022-09-09 17:20:41 +0800

    fix: can't use foreach value from RegExpMatchArray

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -4,6 +4,16 @@ const xrandr = "xrandr";
 
 export const connectedDisplay = async () => {
   const { stdout } = await execa(xrandr)
-  const connected = stdout.match(/.*-\d(?=\s+connected)/); // match "eDP-1" of inside "eDP-1 connected primary screen"
-  return connected ?? "";
+  const connected = stdout.match(/.*-\d(?=\s+connected)/g); // match "eDP-1" of inside "eDP-1 connected primary screen"
+
+  if (connected === null) {
+    return [];
+  }
+
+  const result = []
+  for (let index = 0; index < connected.length; index++) {
+    result.push(connected[index]);
+  }
+
+  return result;
 }
