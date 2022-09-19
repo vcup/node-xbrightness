52fefda672b1d2fb31783f78b57b0e4190aae6c0
miryo <miryo@vcup.moe>
2022-09-19 15:56:48 +0800

    remove declare file

--- src/types/brightness.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getBrightness(): number;
-export declare async function setBrightnessFade(screen: string, brightness: number, time = 3): void;
--- src/types/info.d.ts
+++
@@ -1,3 +0,0 @@
-export declare async function primaryDisplay(): string
-export declare async function connectedDisplay(): string[]
-export declare async function disconnectedDisplay(): string[]
