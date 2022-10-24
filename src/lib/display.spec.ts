54e73374a6a68295409bab988c31a88711beac76
miryo <miryo@vcup.moe>
2022-10-24 17:23:24 +0800

    add method for try set resolution
    rename displaySameAs to SameAs

---
+++ src/lib/display.spec.ts
@@ -0,0 +1,9 @@
+import test from 'ava';
+import { primaryDisplay, getDisplayModes } from "./info.js"
+import { FoundNearResolution } from "./display";
+
+test("FoundNearResolution", t => {
+  const source = primaryDisplay();
+  const mode = FoundNearResolution(source, "1366x728", getDisplayModes(source));
+  t.pass();
+})
--- src/lib/display.ts
+++ src/lib/display.ts
@@ -1,16 +1,43 @@
 import { execSync } from "child_process";
+import { getDisplayModes } from "./info";
+import { mode } from "../types/mode";
 
 const xrandr = "xrandr";
 const o_output = " --output ";
 const o_sameas = " --same-as ";
 const o_mode = " --mode "
 
-export function displaySameAs(source: string, target: string) {
+export function SameAs(source: string, target: string) {
   const cmd = xrandr + o_output + source + o_sameas + target;
   execSync(cmd);
 }
 
-export function displaySetResolution(source: string, mode: string) {
+export function SetResolution(source: string, mode: string) {
   const cmd = xrandr + o_output + source + o_mode + mode;
   execSync(cmd);
+}
+
+export function TrySetResolution(source: string, target: string): mode {
+  const allMode = getDisplayModes(source);
+  const targetMode = FoundNearResolution(source, target, allMode);
+  SetResolution(source, targetMode.Resolution);
+  return targetMode;
+}
+
+export function FoundNearResolution(source: string, target: string, modes: mode[]): mode {
+  if (modes === null || modes.length === 0) throw "parameter 'modes' is null or empty";
+  const diffrences = [] as number[];
+
+  const target_s = target.split('x');
+  const targetDitizing = [Number.parseInt(target_s[0]), Number.parseInt(target_s[1])];
+  for (let index = 0; index < modes.length; index++) {
+    const resolution = modes[index].Resolution.split('x');
+    const diffrence =
+      (Number.parseInt(resolution[0]) - targetDitizing[0]) *
+      (Number.parseInt(resolution[1]) - targetDitizing[1]);
+    diffrences.push(Math.abs(diffrence));
+  }
+
+  const min = Math.min.apply(null, diffrences);
+  return modes[diffrences.indexOf(min)];
 }
