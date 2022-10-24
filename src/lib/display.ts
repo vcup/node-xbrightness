00c7cde7d92d14e97d4d116d9fe1dcbc5f534a50
miryo <miryo@vcup.moe>
2022-10-24 16:29:01 +0800

    add new method to set resolution

--- src/lib/display.ts
+++ src/lib/display.ts
@@ -3,8 +3,14 @@ import { execSync } from "child_process";
 const xrandr = "xrandr";
 const o_output = " --output ";
 const o_sameas = " --same-as ";
+const o_mode = " --mode "
 
 export async function displaySameAs(source: string, target: string) {
   const cmd = xrandr + o_output + source + o_sameas + target;
+  execSync(cmd);
+}
+
+export async function displaySetResolution(source: string, mode: string) {
+  const cmd = xrandr + o_output + source + o_mode + mode;
   execSync(cmd);
 }
